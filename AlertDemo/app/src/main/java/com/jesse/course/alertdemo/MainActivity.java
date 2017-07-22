1eee9c294ef9f7864b63dcf3377d28b104d83d04
Jesse DiGregorio <jesse.digregorio@live.com>
2017-07-22 09:45:37 -0700

    Menu, Alert, and Language preferences demos

---
+++ AlertDemo/app/src/main/java/com/jesse/course/alertdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.jesse.course.alertdemo;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Are you certain?")
+                .setMessage("Really? No turning back.")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this, "It is done!", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("Nope", null)
+                .show();
+
+    }
+}
