6c5cb08daa3dd835993f6a759ab5ad92566fd927
Conner Anderson <conner.c.anderson@nordstrom.com>
2017-07-27 13:37:17 -0700

    Adding retrolambda for Java 1.8 features

--- NewsReader/app/src/main/java/com/jesse/course/newsreader/domain/BaseService.java
+++ NewsReader/app/src/main/java/com/jesse/course/newsreader/domain/BaseService.java
@@ -1,5 +1,7 @@
 package com.jesse.course.newsreader.domain;
 
+import java.util.concurrent.TimeUnit;
+
 import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
@@ -7,6 +9,9 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public abstract class BaseService<InterfaceType> {
+
+    private static final int HTTP_TIMEOUT = 10;
+    private static final TimeUnit HTTP_TIMEOUT_UNITS = TimeUnit.SECONDS;
 
     private final Class<InterfaceType> mInterfaceClass;
     private final String mBaseUrl;
@@ -43,6 +48,12 @@ public abstract class BaseService<InterfaceType> {
     }
 
     private OkHttpClient getOkHttpClient() {
-        return null;
+        if(mOkClient == null) {
+            OkHttpClient.Builder mOkClient = new OkHttpClient.Builder();
+            mOkClient.connectTimeout(HTTP_TIMEOUT, HTTP_TIMEOUT_UNITS);
+            mOkClient.readTimeout(HTTP_TIMEOUT, HTTP_TIMEOUT_UNITS);
+            mOkClient.writeTimeout(HTTP_TIMEOUT, HTTP_TIMEOUT_UNITS);
+        }
+        return mOkClient;
     }
 }
