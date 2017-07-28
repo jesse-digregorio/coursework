94865c64be5c6503f10ce8aea1aafb7728608742
Conner Anderson <conner.c.anderson@nordstrom.com>
2017-07-27 17:58:36 -0700

    Styling

--- NewsReader/app/src/main/java/com/jesse/course/newsreader/domain/NewsReaderApi.java
+++ NewsReader/app/src/main/java/com/jesse/course/newsreader/domain/NewsReaderApi.java
@@ -2,9 +2,9 @@ package com.jesse.course.newsreader.domain;
 
 public class NewsReaderApi extends BaseService<NewsReaderService> {
 
-    public static final String baseUrl = "https://hacker-news.firebaseio.com/";
+    public static final String BASE_URL = "https://hacker-news.firebaseio.com/";
 
     public NewsReaderApi() {
-        super(NewsReaderService.class, baseUrl);
+        super(NewsReaderService.class, BASE_URL);
     }
 }
